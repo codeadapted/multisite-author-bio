19517e4a17b8c5ef50ecfbab8bcc3526df83d7b4
Shawn Peters Briones <shawnpeters93@yahoo.com>
2022-10-05 12:04:06 -0500

    add tags

--- multisite-author-bio.php
+++ multisite-author-bio.php
@@ -2,6 +2,7 @@
 /**
  * Plugin Name:  Multisite Author Bio
  * Description:  Allow user bio to be translated on multisite instance
+ * Tags:         author,author bio,author description,multisite,multisite author,multisite author bio,multisite author description
  * Version:      1.0.0
  * Author:       CodeAdapted
  * Author URI:   https://codeadapted.com
